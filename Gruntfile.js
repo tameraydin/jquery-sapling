5c3c28f82cc218d0600d176c02ed6eb1937428c6
Tamer Aydin <tamer.aydin@gmail.com>
2014-12-07 22:05:43 +0100

    implemented grunt

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('sapling.jquery.json'),
+		banner: '/*! \n\t<%= pkg.title %> <%= pkg.version %> - <%= pkg.homepage %>\n*/\n',
+		usebanner: {
+			dist: {
+				options: {
+					position: 'top',
+					banner: '<%= banner %>'
+				},
+				files: {
+					src: ['jquery.sapling.css']
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>\n'
+			},
+			build: {
+				files: {
+					'jquery.sapling.min.js': ['jquery.sapling.js']
+				}
+			}
+		},
+		jshint: {
+			all: [
+				'jquery.sapling.js'
+			]
+		},
+		watch: {
+			scripts: {
+				files: ['jquery.sapling.js'],
+				tasks: ['jshint','uglify']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-banner');
+
+	grunt.registerTask('default', ['jshint', 'uglify','usebanner']);
+
+};
